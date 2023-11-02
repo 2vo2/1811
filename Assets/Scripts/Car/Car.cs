326aa1d278bff59c12eb2b97dbee719fe2a05bcd
Yehor <egorzvoznikov@gmail.com>
2023-11-02 17:32:08 +0200

    Add loop logic for car

--- Assets/Scripts/Car/Car.cs
+++ Assets/Scripts/Car/Car.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.TryGetComponent(out SpawnPoint spawnPoint))
+        {
+            transform.position = spawnPoint.transform.position;
+        }
+    }
 }
